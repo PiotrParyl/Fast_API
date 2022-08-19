c6c1f3c79821b6f25958574bc139e2c65ddb6bc9
piotr <piotrekzrydek@gmial.com>
2022-08-19 16:57:09 +0200

    Path() is working, POST method working <3

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-from msilib.schema import Patch
-from pathlib import Path
-from fastapi import FastAPI
 
+from fastapi import FastAPI, Path, Query
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
 
 my_data = {
     1:{
@@ -30,7 +36,23 @@ def home():
 def test1():
     return {"data1":"data1"}
 
-@app.get("/get_item/{item_id}")
-def get_item(item_id: int = Path()):
+@app.get("/get-item/{item_id}")
+def get_item(item_id: int = Path(None, description="siema elo test")):
     return my_data[item_id]
 
+@app.get("/get-by-name/")
+def get_item(name: str):
+    for item in my_data:
+        if my_data[item]['name']== name:
+            return my_data[item]
+    return {"Data":"Not founddd"}
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in my_data:
+        return {"Error":"Item ID alredy exists"}
+
+    my_data[item_id] = {"name": item.name, "brand": item.brand, "price": item.price}
+
+    return my_data[item_id]
+    
